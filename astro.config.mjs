cdf70abc349e9a1a1797e4f56e2c514818a52178
eslavidaloca <eslavi_jonhas@hotmail.com>
2025-03-01 09:51:33 -0600

    Added sitemap, robots.txt and expressiveCode

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,11 +8,18 @@ import icon from 'astro-icon';
 
 import vercel from '@astrojs/vercel';
 
+import sitemap from '@astrojs/sitemap';
+
+import robotsTxt from 'astro-robots-txt';
+
+import expressiveCode from 'astro-expressive-code';
+
 // import sentry from '@sentry/astro';
 // import spotlightjs from '@spotlightjs/astro';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://eslavi-cv.vercel.app",
   vite: {
     plugins: [tailwindcss()],
     resolve: {
@@ -25,7 +32,26 @@ export default defineConfig({
 
   integrations: [react({
     experimentalReactChildren: true,
-  }), icon()],
+  }), icon(), sitemap({
+    i18n: {
+      defaultLocale: 'en', // All urls that don't contain `es` after `https://eslavi-cv.vercel.app/` will be treated as default locale, i.e. `en`
+      locales: {
+        en: 'en-US', // The `defaultLocale` value must present in `locales` keys
+        es: 'es-MX',
+      },
+    },
+  }), robotsTxt(), expressiveCode({
+    themes: ['rose-pine', 'rose-pine-dawn'],
+    shiki: {
+      // Example: Only include languages 'astro' and 'sass'
+      // in the bundle, reducing SSR bundle size by 80%
+      bundledLangs: ['astro', 'sass'],
+    },
+    frames: {
+      // Example: Hide the "Copy to clipboard" button
+      showCopyToClipboardButton: false,
+    },
+  })],
   // }), icon(), sentry(), spotlightjs()],
 
   experimental: {
@@ -34,7 +60,6 @@ export default defineConfig({
 
   i18n: {
     defaultLocale: "en",
-    // routing: {prefixDefaultLocale: false},
     locales: ["en", "es"],
   },
 
