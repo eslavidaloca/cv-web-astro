496c5d53b6147e629ca27b0d3886843230092e37
eslavidaloca <eslavi_jonhas@hotmail.com>
2025-03-14 23:55:55 -0600

    Created previous work page

---
+++ src/data/pages/work/previousWork-data.ts
@@ -0,0 +1,30 @@
+export const cardListIcon = [
+	{
+		img: "work/ibm",
+		classIcon: "size-[160px] text-blue-500",
+		title: "SOA",
+		description: "Lead Developer and Technical Writer for the SOA module.",
+		buttonText: "Read more",
+		fullDescription: "In charge of developing any feature our users want to display in their portal (website), and also in charge of updating and maintaining every portal in the network. Currently working in the development of a CI/CD pipeline to update all portals automatically.",
+		technologies: ["Python", "Django", "Wagtail", "Angular", "Typescript", "Bootstrap", "Postgresql", "Docker", "Kubernetes", "AWS ECR", "AWS ECS", "AWS S3"]
+	},
+	{
+		img: "work/ibm",
+		classIcon: "size-[160px] text-blue-500",
+		title: "Technical Writer of different modules",
+		description: "Analyst of different modules to write the documentation of the modules to understand how they work.",
+		buttonText: "Read more",
+		fullDescription: "In charge of developing any feature our users want to display in their portal (website), and also in charge of updating and maintaining every portal in the network. Currently working in the development of a CI/CD pipeline to update all portals automatically.",
+		technologies: ["Python", "Django", "Wagtail", "Angular", "Typescript", "Bootstrap", "Postgresql", "Docker", "Kubernetes", "AWS ECR", "AWS ECS", "AWS S3"]
+	},
+	{
+		img: "work/ibm",
+		classIcon: "size-[160px] text-blue-500",
+		title: "React Dashboard",
+		description: "Collaborator to improve and fix existing features.",
+		buttonText: "Read more",
+		fullDescription: "In charge of developing any feature our users want to display in their portal (website), and also in charge of updating and maintaining every portal in the network. Currently working in the development of a CI/CD pipeline to update all portals automatically.",
+		technologies: ["Python", "Django", "Wagtail", "Angular", "Typescript", "Bootstrap", "Postgresql", "Docker", "Kubernetes", "AWS ECR", "AWS ECS", "AWS S3"]
+	}
+	
+]
---
+++ src/interfaces/CardListIcon.ts
@@ -0,0 +1,9 @@
+export interface CardListIcon {
+    img: string,
+    classIcon: string,
+    title: string,
+    description: string,
+    buttonText: string,
+    fullDescription: string,
+    technologies: string[]
+}
