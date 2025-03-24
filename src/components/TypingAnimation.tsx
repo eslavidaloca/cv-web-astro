1d12588294c9ff6123a16b54de1de3c692fa70e6
eslavidaloca <eslavi_jonhas@hotmail.com>
2025-03-23 22:26:50 -0600

    Reactivy added, content missing

--- src/components/TypingAnimation.tsx
+++ src/components/TypingAnimation.tsx
@@ -1,7 +1,10 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 import { TypingAnimation } from "@/components/magicui/typing-animation";
 
 interface TypingAnimation {
-    text: string;
     className?: string;
     duration?: number;
     delay?: number;
@@ -10,5 +13,30 @@ interface TypingAnimation {
 }
 
 export function TypingAnimationComponent(props: TypingAnimation) {
-    return <TypingAnimation className={props.className} duration={props.duration} delay={props.delay} as={props.as} startOnView={props.startOnView}>{props.text}</TypingAnimation>;
+
+    const [year, setYear] = useState("2025");
+
+    useEffect(() => {
+        const buttons = document.querySelectorAll("button.year-button");
+
+        const handleClick = (event: Event) => {
+            const button = event.currentTarget as HTMLButtonElement;
+            const parent = button.parentElement;
+            const newYear = parent?.getAttribute("data-year") || "2025";
+            setYear(newYear);
+        };
+
+        buttons.forEach((button) => {
+            button.addEventListener("click", handleClick);
+        });
+
+        // Cleanup: Eliminar los event listeners cuando el componente se desmonta
+        return () => {
+            buttons.forEach((button) => {
+                button.removeEventListener("click", handleClick);
+            });
+        };
+    }, []);
+
+    return <TypingAnimation className={props.className} duration={props.duration} delay={props.delay} as={props.as} startOnView={props.startOnView}>{year}</TypingAnimation>;
 }
