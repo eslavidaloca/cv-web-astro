de54548040208b020939e6f41b981a21f9550d20
eslavidaloca <eslavi_jonhas@hotmail.com>
2025-03-24 21:20:09 -0600

    Changed some styles for cards and created the timeline-data

---
+++ src/data/pages/work/timeline-data.ts
@@ -0,0 +1,42 @@
+export const timeline = {
+    2025: {
+
+    },
+    2024: {
+
+    },
+    2023: {
+
+    },
+    2022: {
+
+    },
+    2021: {
+
+    },
+    2020: {
+        title: "",
+    },
+    2019: {
+        title: "Getting started with object-oriented programming",
+        tech: ["C", "C++"],
+        events: [
+            {
+                title: "Object-oriented programming",
+                description: "At this time I started learning how to code in C++ as a language for object-oriented programming."
+            },
+            {
+                title: "BlueXolo framework",
+                description: "A small team collaborated with IBM to develop the BlueXolo framework where I got to work."
+            }
+
+        ]
+    },
+    2018: {
+        title: "Started studying how to code at college",
+        tech: ["Python"],
+        events: [{
+
+        }]
+    }
+}
