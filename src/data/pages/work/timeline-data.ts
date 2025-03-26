c5803c21a8aa104b7e4a6b1c9d12d1406104dbba
eslavidaloca <eslavi_jonhas@hotmail.com>
2025-03-25 23:28:15 -0600

    Kept working on the data

--- src/data/pages/work/timeline-data.ts
+++ src/data/pages/work/timeline-data.ts
@@ -12,14 +12,68 @@ export const timeline = {
 
     },
     2021: {
-
+        title: "Decided to keep improving as a web developer",
+        tech: ["Assembly(Emu 8086)", "C++", "PHP", "Laravel", "HTML", "CSS", "Javascript", "Python", "Django", "Linux", "Proteus"],
+        events: [
+            {
+                title: "Assembly language",
+                description: "My first approach to code in assembly language using the Emu 8086, liked it but was confusing at first."
+            },
+            {
+                title: "Linux distros",
+                description: "Tried different Linux distros to see how different they were between each other, and how easy they were to install through a virtual machine."
+            },
+            {
+                title: "Protoboards with Proteus",
+                description: "Used Proteus to design and simulate circuits in a protoboard."
+            },
+            {
+                title: "Laravel framework",
+                description: "My second framework used and really liked it as I was used to PHP."
+            },
+        ]
     },
     2020: {
-        title: "",
+        title: "Learning new things besides coding as I knew",
+        tech: ["Postgresql", "Oracle DB", "C++", "HTML", "PHP", "CSS", "Javascript", "JQuery", "Python", "Django", "Proteus"],
+        events: [
+            {
+                title: "RSA encryption",
+                description: "Learned how to encrypt data using the RSA algorithm in C++."
+            },
+            {
+                title: "Big O Notation",
+                description: "My first time learning about Big O Notation and why it's important."
+            },
+            {
+                title: "Data recovery",
+                description: "Learned how important data is, how to back it up and ways to restore it in some cases."
+            },
+            {
+                title: "Computer Architecture",
+                description: "Learned about the inner workings of a computer, logic gates, components, etc."
+            },
+            {
+                title: "Technical documentation",
+                description: "Saw how to made technical documentation through multiple diagrams and other tools."
+            },
+            {
+                title: "Automatas and regular expressions",
+                description: "Understood how automatas and regular expressions work and how you can use them."
+            },
+            {
+                title: "Web development",
+                description: "Used the basics of HTML, CSS, and Javascript with PHP to create multiple websites, also used JQuery to make them more interactive."
+            },
+            {
+                title: "BlueXolo framework",
+                description: "Kept working on this project every week, also it was my first approach to a framework as Django."
+            }
+        ]
     },
     2019: {
         title: "Getting started with object-oriented programming",
-        tech: ["C", "C++"],
+        tech: ["C", "C++", "Python"],
         events: [
             {
                 title: "Object-oriented programming",
@@ -33,10 +87,11 @@ export const timeline = {
         ]
     },
     2018: {
-        title: "Started studying how to code at college",
+        title: "Started studying Computer Science at Universidad de Guadalajara",
         tech: ["Python"],
         events: [{
-
+            title: "Wrote my first program on Python",
+            description: "Never wrote a single line of code before college"
         }]
     }
 }
